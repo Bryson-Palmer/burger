dadc30fbb6f46208e60dbb1f8d3bbc315e3fea6e
Bryson Palmer <brysonpalmer@gmail.com>
2021-02-05 23:01:00 -0800

    Added the views folder, files, and initial code. Added the schema and seeds files. Added the config connection.js.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_names) VALUES ('Kimura Chicken Burger');
+INSERT INTO burgers (burger_names) VALUES ('Arm Bar Classic Burger');
+INSERT INTO burgers (burger_names) VALUES ('RNC Bacon Burger');
